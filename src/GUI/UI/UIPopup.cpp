29c4bef3a8cca59f8b919c288e29970bc64e03f1
Baste Nesse Buanes <baste@rain-games.com>
2025-05-29 13:29:10 +0200

    Reverted to hard-coding some values in UIPopup that was comparing scaled values to unscaled ones. Should do this properly later

--- src/GUI/UI/UIPopup.cpp
+++ src/GUI/UI/UIPopup.cpp
@@ -1848,8 +1848,7 @@ void UI_OnMouseRightClick(int mouse_x, int mouse_y) {
             if (GetCurrentMenuID() > MENU_MAIN) break;
             mouse->DoMouseLook();
 
-            if ((signed int)pY > (signed int)pViewport->uScreen_BR_Y) {
-            //if ((signed int)pY > (signed int)pViewport->uScreen_BR_Y) {
+            if ((signed int)pY > 352) {
                 int characterIndex = pX / 118;
                 if (characterIndex < 4) { // portaits zone
                     popup_window.sHint.clear();
@@ -1859,7 +1858,7 @@ void UI_OnMouseRightClick(int mouse_x, int mouse_y) {
                     popup_window.uFrameY = 60;
                     GameUI_CharacterQuickRecord_Draw(&popup_window, characterIndex);
                 }
-            } else if ((int)pX > pViewport->uScreen_BR_X) {
+            } else if ((int)pX > 468) {
                 if (pY >= 130) {
                     if (pX >= 476 && pX <= 636 && pY >= 240 && pY <= 300) {  // buff_tooltip zone
                         drawBuffPopupWindow();
